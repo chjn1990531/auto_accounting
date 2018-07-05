31463f2b2536264a0b1ec590a02c5644496533a3
Jiangning Chen <chjn1990531@gmail.com>
2018-07-05 15:52:50 -0400

    test

--- version1.0/load_mails.py
+++ version1.0/load_mails.py
@@ -6,28 +6,18 @@ import datetime
 
 
 def process_mailbox(M):
-  rv, data = M.search(None, "ALL")
-  print(rv)
-  print(data)
-  # if rv != 'OK':
-  #     print("No messages found!")
-  #     return
-  # 
-  # for num in data[0].split():
-  #     rv, data = M.fetch(num, '(RFC822)')
-  #     if rv != 'OK':
-  #         print("ERROR getting message", num)
-  #         return
-  #
-  #     msg = email.message_from_string(data[0][1])
-  #     print('Message %s: %s' % (num, msg['Subject']))
-  #     print('Raw Date:', msg['Date'])
-  #     date_tuple = email.utils.parsedate_tz(msg['Date'])
-  #     if date_tuple:
-  #         local_date = datetime.datetime.fromtimestamp(
-  #             email.utils.mktime_tz(date_tuple))
-  #         print("Local Date:", \
-  #             local_date.strftime("%a, %d %b %Y %H:%M:%S"))
+    rv, data = M.search(None, "ALL")
+
+    # print(rv)
+    # print(data)
+
+    if rv != 'OK':
+        print("No messages found!")
+        return
+    else:
+        for num in data[0].split():
+            typ, data = M.fetch(num, '(RFC822)')
+            print('Message %sn%sn' % (num, data[0][1]))
 
 
 
@@ -40,10 +30,10 @@ except imaplib.IMAP4.error:
     # ... exit or deal with failure...
 
 
-rv, mailboxes = M.list()
-if rv == 'OK':
-    print("Mailboxes:")
-    print(mailboxes)
+# rv, mailboxes = M.list()
+# if rv == 'OK':
+#     print("Mailboxes:")
+#     print(mailboxes)
 
 rv, data = M.select("INBOX")
 if rv == 'OK':
